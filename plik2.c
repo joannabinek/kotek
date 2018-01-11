c058c9fbfb5a70acde39373aa3f654eda52c6a2a
michalmdc <michal.cieslik17@wp.pl>
2018-01-11 16:21:58 +0100

    commit pli2

---
+++ plik2.c
@@ -0,0 +1,33 @@
+// Sito Eratostenesa
+// Data   : 17.03.2008
+// (C)2012 mgr Jerzy Wa³aszek
+//----------------------------
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+  unsigned int g,i,n,w;
+  bool * S;
+
+  cin >> n;
+  S = new bool[n + 1];
+  for(i = 2; i <= n; i++) S[i] = true;
+  g = (unsigned int)sqrt(n);
+  for(i = 2; i <= g; i++)
+    if(S[i])
+    {
+     w = i * i;
+      while(w <= n)
+      {
+        S[w] = false; w += i;
+      }
+    } 
+  for(i = 2; i <= n; i++) if(S[i]) cout << i << " ";
+  cout << endl;
+  delete [] S;
+  return 0;
+}
